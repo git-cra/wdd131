59f2104ed527a5f527fce42c54723bd00f11a96d
git-cra <timcrager@gmail.com>
2024-12-09 00:43:05 -0600

    temple fixes

--- week04/scripts/filtered-ttemples.js
+++ week04/scripts/filtered-ttemples.js
@@ -22,82 +22,78 @@ const temples = [
 		templeName: "Chicago Illinois",
 		location: "Chicago, IL",
 		dedicated: "1985, August 9",
-		area: "37062 ft²",
+		area: 37062,
 		imageUrl:
 			"https://www.churchofjesuschrist.org/imgs/5dbebad7cd20cfc90592b6acb8c5864db3a98ade/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Manti Utah",
-		location: "Manti, Utah, United States",
-		dedicated: "1888, May, 21",
-		area: "74792 ft²",
+		templeName: "Cebu City Philippines",
+		location: "Cebu City, Philippines",
+		dedicated: "2010, June 13",
+		area: 29556,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+			"https://churchofjesuschrist.org/imgs/751a0dd8846a820488f147fd2b50664be8141efd/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Payson Utah",
-		location: "Payson, Utah, United States",
-		dedicated: "2015, June, 7",
-		area: "74792 ft²",
+		templeName: "Dallas Texas",
+		location: "Dallas, Texas",
+		dedicated: "1984, October 19",
+		area: 44207,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+			"https://www.churchofjesuschrist.org/imgs/167b081b94fddc43d81200a03a9d5fc26c38a770/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Yigo Guam",
-		location: "Yigo, Guam",
-		dedicated: "2020, May, 2",
-		area: "74792 ft²",
+		templeName: "Manila Philippines",
+		location: "Manila, Philippines",
+		dedicated: "1984, September 1984",
+		area: 26683,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+			"https://churchofjesuschrist.org/imgs/de7a4293e088152271f9c6292ee37fbf83799741/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Washington D.C.",
-		location: "Kensington, Maryland, United States",
-		dedicated: "1974, November, 19",
-		area: "74792 ft²",
+		templeName: "Provo Utah",
+		location: "Provo, Utah",
+		dedicated: "1972, February 9",
+		area: 130825,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+			"https://www.churchofjesuschrist.org/imgs/15e3f7bd07d84cbff0cb7440cc9b32253706fc39/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Lima Perú",
-		location: "Lima, Perú",
-		dedicated: "1986, January, 10",
-		area: "74792 ft²",
+		templeName: "Rexburg Idaho",
+		location: "Rexburg, Idaho",
+		dedicated: "2008, February 3",
+		area: 57504,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+			"https://churchofjesuschrist.org/imgs/7b6a4d1244ef80f58e5fc47da75707a8c3237173/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Mexico City Mexico",
-		location: "Mexico City, Mexico",
-		dedicated: "1983, December, 2",
-		area: "74792 ft²",
+		templeName: "Sacramento California",
+		location: "Sacramento, California",
+		dedicated: "2006, September 3",
+		area: 19500,
 		imageUrl:
-			"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+			"https://churchofjesuschrist.org/imgs/f7bec2948b877dc7cbecba7e355d5a6847b52f7e/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Bogotá Colombia",
-		location: "Bogotá, Distrito Capital",
-		dedicated: "1999, Abril, 24",
-		area: "74792 ft²",
-		imageUrl: "images/bogota.webp",
-		//"https://churchofjesuschristtemples.org/bogota-colombia-temple/photographs/#Gallery-12"
+		templeName: "Salt Lake Utah",
+		location: "Salt Lake City, Utah",
+		dedicated: "1893, April 6",
+		area: 253000,
+		imageUrl: "https://www.churchofjesuschrist.org/imgs/7761e87183d3a9d62055ebb8b18035d6f7441789/full/1920%2C/0/default",
 	},
 	{
-		templeName: "Barranquilla Colombia",
-		location: "Barranquilla Atlantico Colombia",
-		dedicated: "2018, Diciembre, 9",
-		area: "74792 ft²",
-		imageUrl: "images/barranquilla-temple.webp"
-		//"https://churchofjesuschristtemples.org/barranquilla-colombia-temple/photographs/#Official-9"
+		templeName: "San Antonio Texas",
+		location: "San Antonio, Texas",
+		dedicated: "2005, May 22",
+		area: 16800,
+		imageUrl: "https://www.churchofjesuschrist.org/imgs/ff8075c9cfce23e3e37d14c00bd996c21ea4d530/full/1920%2C/0/default"
 	},
 	{
-		templeName: "Provo City Center",
-		location: "Provo, Utah United States",
-		dedicated: "2016, March, 20",
-		area: "74792 ft²",
-		imageUrl: "images/provoTemple.webp"
-		//"https://churchofjesuschristtemples.org/provo-city-center-temple/photographs/#Official-13"
-
+		templeName: "Star Valley Wyoming",
+		location: "Afton, WY",
+		dedicated: "2016, October 30",
+		area: 18609,
+		imageUrl: "https://www.churchofjesuschrist.org/imgs/165dc66321dfd7b3f59570883647b3a9476e69a7/full/1920%2C/0/default"
 	},
 ]
 
@@ -154,7 +150,7 @@ largeLink.addEventListener('click', () => {
 const smallLink = document.getElementById("small");
 smallLink.addEventListener('click', () => {
 	const smallTemples = temples.filter(temple => {
-		const size = temple.area < 10000;
+		const size = temple.area < 20000;
 		return size;
 	});
 	rendersections(smallTemples)
