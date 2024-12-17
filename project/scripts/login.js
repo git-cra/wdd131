aeb4e9f347722cf83935b51fd13237e2c2917e9c
git-cra <timcrager@gmail.com>
2024-12-16 21:33:57 -0600

    login js cleanup

--- project/scripts/login.js
+++ project/scripts/login.js
@@ -1,55 +1,8 @@
 const lastModifiedElement = document.getElementById("lastModified");
 const currentYearElement = document.getElementById("year");
 
-
 const currentYear = new Date().getFullYear();
 currentYearElement.textContent = `© ${currentYear} Tim Crager, WI`;
 
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
-
-document.addEventListener("DOMContentLoaded", () =>{
-    const products = [
-        {
-          id: "fc-1888",
-          name: "Flux capacitor",
-          averagerating: 4.5
-        },
-        {
-          id: "ac-2000",
-          name: "Low voltage reactor",
-          averagerating: 3.9
-        },
-        {
-          id: "fc-2050",
-          name: "Power laces",
-          averagerating: 4.7
-        },
-        {
-          id: "fs-1987",
-          name: "Time circuits",
-          averagerating: 3.5
-        },
-        {
-          id: "jj-1969",
-          name: "Warp equalizer",
-          averagerating: 5.0
-        }
-      ];
-    const productSelect = document.getElementById("pl")
-    products.forEach(product => {
-        const option = document.createElement("option");
-        option.value = product.id;
-        option.textContent = product.name;
-        productSelect.appendChild(option);
-    })
-    let reviewCount = parseInt(localStorage.getItem('reviewCount') || '0', 10);
-    localStorage.setItem('reviewCount', reviewCount);
-    document.querySelector('form').onsubmit = function(event){
-        event.preventDefault();
-        reviewCount++;
-        localStorage.setItem('reviewCount', reviewCount);
-        alert(`Review Submitted!: Total reviews: ${reviewCount}`);
-        window.location.href = "review.html";
-    }
-})
