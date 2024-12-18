32c0602cb065e47ce67552c117e3ce4b0820eda9
git-cra <timcrager@gmail.com>
2024-12-17 18:56:50 -0600

    css corrections

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -8,7 +8,11 @@ currentYearElement.textContent = `© ${currentYear} Tim Crager - WI`;
 const lastModifiedDate = document.lastModified;
 lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)){js=d.createElement(s);
+		js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+		fjs.parentNode.insertBefore(js,fjs);
+	}}(document,'script','weatherwidget-io-js');
 
 const mainnav = document.querySelector(".navList")
 const hambutton = document.querySelector('#menu');
